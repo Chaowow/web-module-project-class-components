55c0450278f0a4dae9703dee8d9b9dcfb1803ab2
Frankie <frankiejosefej@gmail.com>
2023-06-22 16:28:08 -0400

    met mvp

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,66 @@
 import React from 'react'
+import Todo from './Todo';
+import TodoList from './TodoList';
+import Form from './Form';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      toDo: [
+        {
+          name: '',
+          id: Date.now(),
+          completed: false
+        },
+      ]
+    };
+  }
+
+  handleToggle = (clickedId) => {
+
+    this.setState({
+      ...this.state,
+      toDo: this.state.toDo.map(todo => {
+        if(todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        } 
+        
+        return todo;
+      })
+    })
+  }
+
+  handleAdd = (name) => {
+    const newTodo = {
+      name: name,
+      id: Date.now(),
+      completed: false
+    };
+    this.setState({
+      ...this.state,
+      toDo: [...this.state.toDo, newTodo]
+    });
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      toDo: this.state.toDo.filter(todo => !todo.completed)
+    });
+  }
+  
   render() {
+    const { toDo } = this.state;
     return (
       <div>
-        Todo App
+        <h2>TODOS</h2>
+        <TodoList handleToggle={this.handleToggle} todos={toDo} />
+        <Form handleAdd={this.handleAdd} />
+        <button onClick={this.handleClear}>Clear</button> 
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,36 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: ''
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.handleAdd(this.state.input);
+    this.setState({
+      input: ''
+    });
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      ...this.state,
+      input: e.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
+    <div>
+      <form>
+        <input type='text' placeholder='Add to do' value={this.state.input} onChange={this.handleInputChange}/>
+        <button onClick={this.handleSubmit}>Add</button>
+      </form>
+    </div>
     )
   }
 }
